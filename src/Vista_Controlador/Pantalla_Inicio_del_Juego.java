8260b2047b2c7d010b6d1237867d68630c5cc184
asierla <2001uri15@gmail.com>
2023-03-16 16:23:41 +0100

    Se ha editado los Diseños de los JFrames.

--- src/Vista_Controlador/Pantalla_Inicio_del_Juego.java
+++ src/Vista_Controlador/Pantalla_Inicio_del_Juego.java
@@ -5,10 +5,23 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import javax.swing.JTextField;
+import java.awt.Toolkit;
+import javax.swing.JButton;
 
 public class Pantalla_Inicio_del_Juego extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textJugadores;
+	private JTextField txtNPCs;
+	private JTextField textMiliSec;
+	private JTextField textPokemon;
 
 	/**
 	 * Launch the application.
@@ -30,12 +43,77 @@ public class Pantalla_Inicio_del_Juego extends JFrame {
 	 * Create the frame.
 	 */
 	public Pantalla_Inicio_del_Juego() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("/home/asierla/git/Pokemon/sprites/pokebol.png"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setTitle("Pokemon - Charged Attack");
+		setBounds(100, 100, 800, 300);
+		setResizable(false);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel Panel_derecho = new JPanel();
+		contentPane.add(Panel_derecho, BorderLayout.EAST);
+		Panel_derecho.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JPanel Panel_dech_superior = new JPanel();
+		Panel_derecho.add(Panel_dech_superior);
+		Panel_dech_superior.setLayout(new GridLayout(4, 2, 0, 0));
+		
+		JLabel lblNewLabel_1 = new JLabel("Jugadores");
+		Panel_dech_superior.add(lblNewLabel_1);
+		
+		textJugadores = new JTextField();
+		textJugadores.setText("");
+		Panel_dech_superior.add(textJugadores);
+		textJugadores.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("NPCs");
+		Panel_dech_superior.add(lblNewLabel_2);
+		
+		txtNPCs = new JTextField();
+		Panel_dech_superior.add(txtNPCs);
+		txtNPCs.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("niliSecs/round");
+		Panel_dech_superior.add(lblNewLabel_3);
+		
+		textMiliSec = new JTextField();
+		Panel_dech_superior.add(textMiliSec);
+		textMiliSec.setColumns(10);
+		
+		JLabel lblPokemons = new JLabel("Pokemons");
+		Panel_dech_superior.add(lblPokemons);
+		
+		textPokemon = new JTextField();
+		Panel_dech_superior.add(textPokemon);
+		textPokemon.setColumns(10);
+		
+		JPanel Panel_dech_inf = new JPanel();
+		Panel_derecho.add(Panel_dech_inf);
+		Panel_dech_inf.setLayout(null);
+		
+		JButton btnReadme = new JButton("ReadMe");
+		btnReadme.setBounds(117, 33, 99, 25);
+		Panel_dech_inf.add(btnReadme);
+		
+		JButton btnFree4all = new JButton("Free4all");
+		btnFree4all.setBounds(12, 33, 99, 25);
+		Panel_dech_inf.add(btnFree4all);
+		
+		JButton btnJugar = new JButton("Jugar");
+		btnJugar.setBounds(56, 89, 117, 25);
+		Panel_dech_inf.add(btnJugar);
+		
+		JPanel Panel_centro = new JPanel();
+		Panel_centro.setBackground(new Color(255, 255, 255));
+		contentPane.add(Panel_centro, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("/home/asierla/git/Pokemon/sprites/main.png"));
+		Panel_centro.add(lblNewLabel);
 	}
-
 }
---
+++ src/Vista_Controlador/Vista_Juego.java
@@ -0,0 +1,49 @@
+package Vista_Controlador;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.GridBagLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+
+public class Vista_Juego extends JFrame {
+
+	private JPanel contentPane;
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Vista_Juego frame = new Vista_Juego("a", 1);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Vista_Juego(String pNom, int pPokemon) {
+		setTitle("Pokemon [Charged Attack] - ");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 300);
+		setResizable(false);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(1, 4, 0, 0));
+	}
+}
