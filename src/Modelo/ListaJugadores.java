4c11beb7d932b2b4e62eb84ef8c99d229d76b8ee
ander <ander@10.109.36.4>
2023-03-24 10:16:46 +0100

    thfjiygi

--- src/Modelo/ListaJugadores.java
+++ src/Modelo/ListaJugadores.java
@@ -74,7 +74,7 @@ public class ListaJugadores extends Observable{
 	public int jugadorGanador() {
 		int id = -1;
 		if (miListaJugadores.finJuego()) {
-			//id = lJugadores.stream().filter(p->!p.haPerdido()).toList().get(0).getId();
+			id = lJugadores.stream().filter(p->!p.haPerdido()).toList().get(0).getId();
 			id =  lJugadores.stream().filter(p->!p.haPerdido()).mapToInt(p->p.getId()).toArray()[0];
 		}
 		return id;
