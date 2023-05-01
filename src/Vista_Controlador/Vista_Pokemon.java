e7355db2c6eba25d436846a4807632477a00ab0a
bibi2di <lnbidane@gmail.com>
2023-05-01 21:22:49 +0200

    limpieza

--- src/Vista_Controlador/Vista_Pokemon.java
+++ src/Vista_Controlador/Vista_Pokemon.java
@@ -16,18 +16,6 @@ public class Vista_Pokemon extends JPanel /*implements Observer */{
 	//private Controler controler = null;
 	private JPanel PanelPokemons;
 	private JPanel PanelPokemons_Vis;
-	/*private JPanel Panel_Pokemons_Inf_Health;
-	private JPanel Panel_Pokemons_Inf_ChargedAttack;
-	private JLabel lbl_Health_bar;
-	private JLabel lblChargedAttack;
-	private JLabel label_1;
-	private JLabel lblAtt; 
-	private JLabel lab_att;
-	private JLabel lblDef;
-	private JLabel lab_Def;
-	private JLabel lblHealth_1;
-	private JLabel lblType;
-	private JLabel lab_type;*/
 	private JPanel Panel_Pokemons_Sup;
 	private JPanel Panel_Pokemons_Cen;
 	private JPanel Panel_Pokemons_Inf;
@@ -58,9 +46,6 @@ public class Vista_Pokemon extends JPanel /*implements Observer */{
 			PanelPokemons_Vis.add(getPanel_Pokemons_Sup(Panel_Pok), BorderLayout.NORTH);
 			PanelPokemons_Vis.add(getPanel_Pokemons_Cen(Panel_Pok), BorderLayout.CENTER);
 			PanelPokemons_Vis.add(getPanel_Pokemons_Inf(Panel_Pok), BorderLayout.SOUTH);
-			//getControler().atributos(i, nJug);
-			
-			
 			panelPok.add(PanelPokemons_Vis);
 		
 			
@@ -93,107 +78,4 @@ public class Vista_Pokemon extends JPanel /*implements Observer */{
 		Panel_Pokemons_Inf.add(pPok.getPanel_Pokemons_Inf_ChargedAttack());
 		return Panel_Pokemons_Inf;
 	}
-	
-	/*public JPanel getPanel_Pokemons_Inf_Health() {
-		Panel_Pokemons_Inf_Health = new JPanel();
-		Panel_Pokemons_Inf_Health.add(getLbl_Health_bar());
-		return Panel_Pokemons_Inf_Health;
-	}
-	public JPanel getPanel_Pokemons_Inf_ChargedAttack() {
-		Panel_Pokemons_Inf_ChargedAttack = new JPanel();
-		Panel_Pokemons_Inf_ChargedAttack.add(getLblChargedAttack());
-		return Panel_Pokemons_Inf_ChargedAttack;
-	}
-	public JLabel getLbl_Health_bar() {
-		lbl_Health_bar = new JLabel("Health");
-		return lbl_Health_bar;
-	}
-	public JLabel getLblChargedAttack() {
-		lblChargedAttack = new JLabel("Charged Attack");
-		return lblChargedAttack;
-	}
-	public JLabel getLabel_1() {
-		String imagenes[] = {"sprites/abra.png", "sprites/alakazam.png", "sprites/butterfree.png", "sprites/caterpie.png"
-				,"sprites/dragonair.png", "sprites/dragonite.png", "sprites/dratini.png", "sprites/gastly.png", "sprites/gengar.png"
-				,"sprites/geodude.png", "sprites/golem.png", "sprites/graveler.png", "sprites/haunter.png", "sprites/kadabra.png",
-				"sprites/machamp.png", "sprites/machoke.png", "sprites/machop.png", "sprites/metapod.png", "sprites/pidgeot.png",
-				"sprites/pidgeotto.png", "sprites/pidgey.png"};
-		label_1 = new JLabel("");
-		int r = (int)(Math.random()*21); 
-		String url = imagenes[r];
-		label_1.setIcon(new ImageIcon(url));
-		return label_1;
-	}
-	public JLabel getLblAtt() {
-		lblAtt = new JLabel("att: ");
-		return lblAtt;
-	}
-	public JLabel getLab_att() {
-		lab_att = new JLabel("??");
-		
-		return lab_att;
-	}
-	public JLabel getLblDef() {
-		lblDef = new JLabel("def: ");
-		return lblDef;
-	}
-	public JLabel getLabel_2_1() {
-		lab_Def = new JLabel("??");
-		return lab_Def;
-	}
-	public JLabel getLblHealth_1() {
-		lblHealth_1 = new JLabel("Health: ");
-		return lblHealth_1;
-	}
-	public JLabel getLab_health() {
-		lab_health = new JLabel("??");
-		return lab_health;
-	}
-	public JLabel getLblType() {
-		lblType = new JLabel("type: ");
-		return lblType;
-	}
-	public JLabel getLab_type() {
-		lab_type = new JLabel("Normal");
-		return lab_type;
-	}
-	*/
-	
-
-	/*private Controler getControler() {
-		if(controler == null) {
-			controler = new Controler();
-		}
-		return controler;
-	}
-	
-	private class Controler implements MouseListener{
-		@Override
-		public void mouseClicked(MouseEvent e) {
-			CampoDeBatalla.getCampoBatalla().sePuedeAtacar(idPok, idJug);
-			//System.out.println("a");
-		}
-		
-		
-		/*public void atributos(int numPok, int nJug) {
-				lab_att.setText(String.valueOf(ListaJugadores.getListaJugadores().buscarJugador(nJug).getPokemon(numPok).getAtaque()));
-				lab_Def.setText(String.valueOf(ListaJugadores.getListaJugadores().buscarJugador(nJug).getPokemon(numPok).getDefensa()));
-				lab_health.setText(String.valueOf(ListaJugadores.getListaJugadores().buscarJugador(nJug).getPokemon(numPok).getVida()));
-		}
-		@Override
-		public void mousePressed(MouseEvent e) {
-			
-		}
-		@Override
-		public void mouseReleased(MouseEvent e) {
-			
-		}
-		@Override
-		public void mouseEntered(MouseEvent e) {
-			
-		}
-		@Override
-		public void mouseExited(MouseEvent e) {			
-		}
-		}*/
 		}
